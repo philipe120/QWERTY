17dc8af61a8789c010cbcf95e244ec886827b86d
Philip Zhu <39408780+philipe120@users.noreply.github.com>
2020-07-16 19:27:40 -0400

    Original Code
    Written during the PCHACKS Hackathon in an attempt to build a distance tracking algorithm for hand detection. (2019)

--- Qwerty.py
+++ Qwerty.py
@@ -1,1 +1,45 @@
-Testing
+const int trgpin = 9;
+const int echopin = 10;
+
+int counter = 0;
+long time;
+int distance; 
+
+void setup(){
+pinMode(trgpin, OUTPUT);
+pinMode(echopin, INPUT);
+Serial.begin(9600);
+}
+
+
+void distance(){
+digitalWrite(trgpin, LOW);
+delayMicroseconds(3);
+digitalWrite(trgpin, HIGH);
+delayMicroseconds(12);
+digitalWrite(trgpin, LOW);
+
+time = pulseIn(echopin, HIGH);
+
+distance = (time*0.0343)/2;
+
+Serial.print("Distance: ");
+Serial.println(distance);
+
+
+}
+
+void loop(){
+
+
+
+
+
+}
+
+    
+
+
+
+
+
